3a567a66e3cea084768fc5916462384dd42227b9
matlearns <navin.lbef@gmail.com>
2025-05-15 15:51:41 +0545

    First Dynamic

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home, about, contact
+from .views import home, about, contact, test
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-]
+    path('test/', test, name='test'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Blog_All
+
 def home(request):
     return render(request, 'blog_app/home.html')
 
@@ -10,4 +12,7 @@ def about(request):
 
 def contact(request):
     return render(request, 'blog_app/contact.html')
-        
+
+def test(request):
+    context = Blog_All.objects.all()
+    return render(request, 'blog_app/test.html', {'context' : context})        
